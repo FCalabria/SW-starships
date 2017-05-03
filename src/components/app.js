3a7ea619fc57e5e0ac16184911cfb92229281dfb
fcalabria <pcalabria@gmail.com>
2017-05-03 22:47:55 +0200

    Move starships_list.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import StarshipsList from '../containers/starships_list';
+import StarshipsList from './starships_list';
 import LoadMore from '../components/load_more';
 import {bindActionCreators} from 'redux';
 import {fetchStarships} from '../actions/index';
