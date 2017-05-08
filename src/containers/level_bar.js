4162904da1ae3aa34f84a3a2a90e390eb61f48ad
fcalabria <pcalabria@gmail.com>
2017-05-08 22:44:13 +0200

    Slightly improved level bar

--- src/containers/level_bar.js
+++ src/containers/level_bar.js
@@ -13,14 +13,16 @@ export default ({name, min, max, level}) => {
     minBar: 10
   }
   if (!isNaN(level)) {
+    const fullBarWidth = svgConstants.levelBarWidth + svgConstants.lowBarWidth;
     let levelDependantTop = level !== -1 ? ((level - min) * (svgConstants.levelBarWidth - svgConstants.angle) / (max - min)) + svgConstants.angle: svgConstants.angle;
-    if (levelDependantTop === svgConstants.angle) levelDependantTop += svgConstants.minBar;
+    if (levelDependantTop <= svgConstants.angle + svgConstants.minBar) levelDependantTop = svgConstants.angle + svgConstants.minBar
     const levelDependantBottom = levelDependantTop - svgConstants.angle;
 
     const backgroundPath = `M${svgConstants.lowBarWidth} ${svgConstants.height} L${svgConstants.angle + svgConstants.lowBarWidth} 0 H ${levelDependantTop + svgConstants.lowBarWidth} L${levelDependantBottom + svgConstants.lowBarWidth} 30 Z`
+    const fullPath = `M${svgConstants.angle + svgConstants.lowBarWidth} 0 H ${fullBarWidth} L${fullBarWidth - svgConstants.angle} 30 L${svgConstants.lowBarWidth} ${svgConstants.height}`
     return (
       <div className="level-bar">
-        <svg width={svgConstants.levelBarWidth + svgConstants.lowBarWidth} height={svgConstants.height} xmlns="http://www.w3.org/2000/svg">
+        <svg width={fullBarWidth} height={svgConstants.height} xmlns="http://www.w3.org/2000/svg">
           <defs>
             <linearGradient id="gradient" x1="0.1" x2="1" y1="0" y2="0.15">
               <stop offset="0%" stopColor="red"/>
@@ -30,9 +32,14 @@ export default ({name, min, max, level}) => {
             <clipPath id="clip">
               <path d={backgroundPath} fill="transparent"/>
             </clipPath>
+            <filter id="background">
+              <feGaussianBlur in="StrokePaint" stdDeviation="3" />
+            </filter>
           </defs>
           <rect x="0" y={svgConstants.height - svgConstants.lowBarHeight} width={svgConstants.lowBarWidth + 5} height={svgConstants.lowBarHeight} fill="red"/>
           <rect x={svgConstants.lowBarWidth} y="0" width={svgConstants.levelBarWidth} height={svgConstants.height} fill="url(#gradient)" clipPath="url(#clip)"/>
+          <path d={fullPath} fill="black" opacity="0.1"/>
+          <path d={fullPath} stroke="black" strokeWidth="2" fill="transparent" filter="url(#background)"/>
           <text x="0" y={svgConstants.height - svgConstants.lowBarHeight - 5} className="name-label">{name}</text>
           <text x={svgConstants.lowBarWidth + svgConstants.minBar * 3} y={svgConstants.height - svgConstants.lowBarHeight - 5} className="level-value">{level}</text>
         </svg>
