32e0da6b1c5838ea6b890e2f4ea15613ccb49833
fcalabria <pcalabria@gmail.com>
2017-05-12 18:54:49 +0200

    Add fetchPhotos action and run on starship_detail

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 const ROOT_URL = 'http://swapi.co/api/starships/';
+const GOOGLE_URL = 'https://www.googleapis.com/customsearch/v1'
+const KEY = 'AIzaSyD9k-K9U9RWMzbCv3gTy_SFXZAWy1GjIsQ';
+const CX = '010600706209839140863:tp5xiakrasi';
 
 export const FETCH_STARSHIPS = 'FETCH_STARSHIPS';
 export const FETCH_STARSHIP_DETAIL = 'FETCH_STARSHIP_DETAIL';
+export const FETCH_PHOTOS = 'FETCH_PHOTOS';
 
 export function fetchStarships(nextUrl = ROOT_URL) {
   const request = axios.get(nextUrl);
@@ -19,4 +23,8 @@ export function fetchStarshipDetail(id) {
     type: FETCH_STARSHIP_DETAIL,
     payload: request
   }
+}
+
+export function fetchPhotos(searchTerm) {
+  return axios.get(`${GOOGLE_URL}?key=${KEY}&cx=${CX}&q=${searchTerm}&fileType=png+jpg`);
 }
--- src/components/starship_detail.js
+++ src/components/starship_detail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { fetchStarshipDetail } from '../actions/index';
+import { fetchPhotos } from '../actions/index';
 import ManufacturerLogo from '../containers/manufacturer_logo';
 import DetailsTable from '../containers/details_table';
 import Starship from '../classes/starship.js';
@@ -12,7 +13,10 @@ class StarshipDetail extends Component {
   constructor(props) {
     super(props);
     this.ID = props.match.params.id
+    this.photos = [];
     this.defineStarship();
+    this.catchPhotos = this.catchPhotos.bind(this);
+    this.handlePhotos = this.handlePhotos.bind(this);
   }
 
   componentWillMount() {
@@ -25,6 +29,31 @@ class StarshipDetail extends Component {
 
   defineStarship(data) {
     this.starship = new Starship(data);
+    if (this.starship.name) {
+      this.props.fetchPhotos(this.starship.name)
+      .then(this.handlePhotos)
+      .catch(this.catchPhotos);
+    }
+  }
+
+  handlePhotos(data) {
+    this.photos = data.items.map(item => item.pagemap.cse_thumbnail[0]);
+  }
+
+  catchPhotos(error) {
+    this.photos = [{
+      src: 'http://only-paper.ru/_nw/94/90619030.jpg',
+      width: 520,
+      height: 358
+    }, {
+      src: 'http://i.imgur.com/gzu2WyQ.jpg',
+      width: 1400,
+      height: 577
+    }, {
+      src: 'https://s-media-cache-ak0.pinimg.com/originals/1b/5f/b3/1b5fb3faeb3b13d7b8d6c3d7221e8328.jpg',
+      width: 1280,
+      height: 720
+    }];
   }
 
   render() {
@@ -42,7 +71,7 @@ class StarshipDetail extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchStarshipDetail }, dispatch);
+  return bindActionCreators({ fetchStarshipDetail, fetchPhotos}, dispatch);
 }
 
 function mapStateToProps({ starships }) {
