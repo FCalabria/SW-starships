64daf486eac3c5706f5dfa91fd9c6c07b1a91665
fcalabria <pcalabria@gmail.com>
2017-08-11 18:14:11 +0200

    Set node env to prod

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const Server = require('./server.js')
 const port = (process.env.PORT || 8080)
 const app = Server.app()
-
+console.log('Starting, environment set to ' + process.env.NODE_ENV);
 if (process.env.NODE_ENV !== 'production') {
+  console.log('Starting server');
   const webpack = require('webpack')
   const webpackDevMiddleware = require('webpack-dev-middleware')
   const webpackHotMiddleware = require('webpack-hot-middleware')
   const config = require('../webpack.config.js')
   const compiler = webpack(config)
-
+  console.log('End of requires');
   app.use(webpackHotMiddleware(compiler))
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
